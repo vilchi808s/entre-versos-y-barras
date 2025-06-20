ba8d324b66cc9f206b9402915e9cb1cc5472dc8f
vilchi808s <emilianomvilchis@gmail.com>
2025-06-19 22:58:25 -0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Entre versos y barras</h1>
-      <p>Bienvenido al análisis completo de letras y barras. Próximamente mucho más.</p>
-    </main>
-  );
-}
