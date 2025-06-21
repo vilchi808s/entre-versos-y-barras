eb392fde74b554b2b61b1c6f275ec6c8fda44db5
vilchi808s <emilianomvilchis@gmail.com>
2025-06-20 18:54:08 -0600

    Actualizar VersoExplicado.js

--- components/VersoExplicado.js
+++ components/VersoExplicado.js
@@ -20,7 +20,7 @@ export default function Talking() {
 
       <VersoExplicado verso="a lo Hacksaw saco Chainsaws" explicacion="‘Hacksaw’ hace referencia a la película 'Hacksaw Ridge', sobre un soldado que salva vidas sin disparar un solo tiro. Pero Ritter da el giro: él saca 'chainsaws', sierras eléctricas, sugiriendo agresividad artística. Está listo para cortar con todo, para destruir estructuras. Es una declaración de guerra poética." />
       
-      <VersoExplicado verso="\"ey suck\" a lo asap rocky" explicacion="Imita el flow y dicción de A$AP Rocky, conocido por su estilo arrastrado y melódico. 'Ey suck' suena como una crítica disimulada ('you suck'), dicho con gracia. Juega con la forma y el fondo: una burla escondida en un homenaje estilístico. Sutil, pero afilado." />
+      <VersoExplicado verso='"ey suck" a lo asap rocky' explicacion="Imita el flow y dicción de A$AP Rocky, conocido por su estilo arrastrado y melódico. 'Ey suck' suena como una crítica disimulada ('you suck'), dicho con gracia. Juega con la forma y el fondo: una burla escondida en un homenaje estilístico. Sutil, pero afilado." />
       
       <VersoExplicado verso="les falta las guirnaldas del shopping y ya tienen hecho el videoclip y el outfit" explicacion="Una joya de crítica. Dice que muchos tienen ya el look de videoclip con solo comprarse adornos del shopping. No tienen contenido, pero lucen como estrellas. Ridiculiza el consumismo superficial en el rap actual. Y la musicalidad de ‘guirnaldas del shopping’ alarga la ironía con ritmo." />
 
